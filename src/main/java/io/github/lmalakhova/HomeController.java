038491b21fa34868caffe5585dcf70f5667ea8fc
lmalakhova <lmalakhova0307@gmail.com>
2018-11-30 16:12:45 +0200

    Initial code 1.Add Controller Class prototype. 2. Add simple View template.

---
+++ src/main/java/io/github/lmalakhova/HomeController.java
@@ -0,0 +1,12 @@
+package io.github.lmalakhova;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
